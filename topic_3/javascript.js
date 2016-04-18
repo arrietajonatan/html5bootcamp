0267d7b3442560a98b89d23dbfbc9172a9d2e7cd
arrietajonatan <arrietajonatan@gmail.com>
2016-04-18 15:14:39 -0300

    I changed little things

--- topic_3/javascript.js
+++ topic_3/javascript.js
@@ -12,37 +12,42 @@ movies.controller("maintController",function($scope,localStorageService){
 				name: "The impossible",
 				year: "2012",
 				duration: "113 min",
-				genre: "drama"
+				genre: "drama",
+				file: "media/piano.jpg"
 			},
 			{
 				name: "The day after tomorrow",
 				year: "2004",
 				duration: "124 min",
-				genre: "science fiction"
+				genre: "science fiction",
+				file: "media/piano.jpg"
 
 			},
 			{
 				name: "The hunger games",
 				year: "2012",
 				duration: "142 min",
-				genre: "science fiction"
+				genre: "science fiction",
+				file: "media/piano.jpg"
 			},
 			{
 				name: "The pianist",
 				year: "2002",
 				duration: "152 min",
-				genre: "drama"
+				genre: "drama",
+				file: "media/piano.jpg"
 			},
 			{
 				name: "American Sniper",
 				year: "2014",
 				duration: "132 min",
-				genre: "drama"
+				genre: "drama",
+				file: "media/piano.jpg"
 			}
 		];
 
 	    }
-   
+   	   
 	    //Mostrando datos del select
 	    $scope.selected=false;
 	    $scope.showSelected=function(){
@@ -56,14 +61,14 @@ movies.controller("maintController",function($scope,localStorageService){
 			localStorageService.set("angular-movies",$scope.movies);
 		    $scope.add=false;
 		    alert('The movie has been added');
-	     }
+ 	     }
          //Borrando y reseteando
 		$scope.deleteMovie = function(movie){
         var index = $scope.movies.indexOf(movie);
-        if(index == -1) return;
-        $scope.movies.splice(index,1);
-        localStorageService.set("angular-movies",$scope.movies);
-        alert('The movie has been deleted');
+        	if(index == -1) return;
+        	$scope.movies.splice(index,1);
+        	 localStorageService.set("angular-movies",$scope.movies);
+       		 alert('The movie has been deleted');
         }
         
         //Mostrar el editar
@@ -79,13 +84,20 @@ movies.controller("maintController",function($scope,localStorageService){
         $scope.edit=false;
         }
 
+        //Cancelar
+        $scope.cancelMovie=function(){
+        $scope.add=false;
+        $scope.edit=false;
+        }
+
         //Editando una pelicula        
   	    $scope.editMovie = function(movie){
         var index = $scope.movies.indexOf(movie);
         var modif = $scope.movies[index];	    	
         modif = $scope.movie;
 	    localStorageService.set("angular-movies",$scope.movies);
-	    $scope.edit=false;
 	    alert('The movie has been edited');
+	    $scope.edit=false;
+	    $scope.show = true;
 	    }
 });
